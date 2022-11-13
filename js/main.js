e960c1de655cb606d5fbd6062fb83bcc2b292e7a
juyai <fue21007@byui.edu>
2022-11-12 23:35:59 -0500

    update

--- js/main.js
+++ js/main.js
@@ -39,6 +39,11 @@ const links = [
   {
     label: "Week 8 notes",
     url: "week8/index.html"
+  },
+
+  {
+    label: "Week 9 notes",
+    url: "week9/team-activity/index.html"
   }
   ]
 
---
+++ week8/script.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+context.strokeStyle = "red";
+context.fillStyle = "blue";
+context.fillRect(10, 10, 100, 100);   
+context.strokeRect(10, 10, 100, 100);
